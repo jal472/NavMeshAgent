852ed793aa3194d92fa0c4f599db520a50fa1098
jal472 <jal472@scarletmail.rutgers.edu>
2018-09-18 17:25:25 -0400

    Made camera follow player so you can move all around the map

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    [SerializeField] private Transform player;
+    [SerializeField] private float smoothSpeed = 0.125f;
+    [SerializeField] private Vector3 offset;
+
+    void LateUpdate()
+    {
+        transform.position = player.position + offset;
+    }
+
+}
