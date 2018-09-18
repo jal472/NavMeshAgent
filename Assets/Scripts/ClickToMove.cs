8f208860e1bae7f1f99634675051ab25caf1bad2
jal472 <jal472@scarletmail.rutgers.edu>
2018-09-18 12:57:59 -0400

    Adding NavMeshAgent to local repo to be added to github later

---
+++ Assets/Scripts/ClickToMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ClickToMove : MonoBehaviour {
+    private Animator animator;
+    private NavMeshAgent navMeshAgent;
+    private bool running = false;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                navMeshAgent.destination = hit.point;
+            }
+        }
+
+        if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
+        {
+            running = false;
+        }
+        else
+        {
+            running = true;
+        }
+
+        animator.SetBool("running", running);
+	}
+}
