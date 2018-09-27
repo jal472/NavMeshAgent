828cb383fbda49ae2756701bdde4ab00a6bddb12
Roman Stashchyshyn <rstash96@gmail.com>
2018-09-27 01:01:21 -0400

    Adding panic state that is triggered by holding down p key on keyboard as well as a random location generator in the navmesh

--- Assets/Scripts/ClickToMove.cs
+++ Assets/Scripts/ClickToMove.cs
@@ -7,6 +7,7 @@ public class ClickToMove : MonoBehaviour {
     private Animator animator;
     private NavMeshAgent navMeshAgent;
     private bool running = false;
+    public panicTrigger script;
 
 	// Use this for initialization
 	void Start () {
@@ -18,14 +19,17 @@ public class ClickToMove : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //Ray ray2 = Camera.main.ScreenPointToRay(Input.mousePosition);
 
         RaycastHit hit;
+
+  
         if (Input.GetMouseButtonDown(0))
         {
-            if (Physics.Raycast(ray, out hit, 100))
-            {
-                navMeshAgent.destination = hit.point;
-            }
+           if (Physics.Raycast(ray, out hit, 100))
+           {
+              navMeshAgent.destination = hit.point;
+           }
         }
 
         if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
@@ -37,6 +41,38 @@ public class ClickToMove : MonoBehaviour {
             running = true;
         }
 
+        // the line below is what is causing the console errors
+        // it has something to do with the inspector and instantiating the object but i coudlnt figure it out yet
+        // once we can get the console message to not happen ADD THIS INSIDE OF THE IF STATEMENT :
+                        //if (script.panicExists == true) {
+                        //navMeshAgent.SetDestination(RandomNavmeshLocation(4f)); }
+        // that line of code should make the soldier go to a random location on the map
+
+        if (script.panicExists)
+        {
+            Debug.Log("ok we got here");
+        }
+
+        //RaycastHit hit2;
+
+
         animator.SetBool("running", running);
 	}
+
+
+    // navmeshagent random location generator
+    public Vector3 RandomNavmeshLocation(float radius)
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * radius;
+        randomDirection += transform.position;
+        NavMeshHit hit;
+        Vector3 finalPosition = Vector3.zero;
+        if (NavMesh.SamplePosition(randomDirection, out hit, radius, 1))
+        {
+            finalPosition = hit.position;
+        }
+        return finalPosition;
+    }
+
+       
 }
---
+++ Assets/Scripts/panicTrigger.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class panicTrigger : MonoBehaviour {
+
+    public static UnityAction trigerPanicAndRun;
+    public static Transform objTransform;
+    bool readyForPanic;
+    public bool panicExists;
+    // Use this for initialization
+    void Start () {
+        objTransform = this.transform;
+        // readyForPanic = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // allow the panic state to be activated while the p-key is being held down
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Debug.Log("panic = true");
+            readyForPanic = true;
+        }
+
+        // once the p-key is released, panic mode can no longer be activated by right click
+        if (Input.GetKeyUp(KeyCode.P))
+        {
+            Debug.Log("panic = false");
+            readyForPanic = false;
+        }
+
+        if (readyForPanic)
+		    if (Input.GetMouseButton(1))
+            {
+                panicExists = true;
+                Debug.Log("Right mouse button has been clicked");
+            }
+	}
+}
